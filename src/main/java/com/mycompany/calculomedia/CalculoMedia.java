5a13b0ea6e3f9bf551f2a84acfa2a8852c775e03
juliano-ss <jotaliano@live.com>
2023-04-11 20:14:04 -0300

    Calculando média simples

---
+++ src/main/java/com/mycompany/calculomedia/CalculoMedia.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calculomedia;
+
+import java.util.Scanner;
+/**
+ *
+ * @author jotal
+ */
+public class CalculoMedia {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        double nota1, nota2, nota3, media;
+
+        System.out.print("Digite a primeira nota: ");
+        nota1 = input.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        nota2 = input.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        nota3 = input.nextDouble();
+
+        media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A média do aluno é: " + media);
+    }
+}
